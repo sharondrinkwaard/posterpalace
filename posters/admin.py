668168d2ae8c555173f094bdf11fe62600c758bf
sharondrinkwaard <97448501+sharondrinkwaard@users.noreply.github.com>
2024-02-22 12:52:45 +0000

    Organize/customize admin

--- posters/admin.py
+++ posters/admin.py
@@ -1,3 +1,21 @@
 from django.contrib import admin
+from .models import Poster, Category
 
-# Register your models here.
+class PosterAdmin(admin.ModelAdmin):
+    list_display = (
+        'name',
+        'category',
+        'price',
+        'sku',
+        'image',
+    )
+
+    ordering = ('name',)
+
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = (
+        'friendly_name',
+    )
+
+admin.site.register(Poster, PosterAdmin)
+admin.site.register(Category, CategoryAdmin)
--- posters/models.py
+++ posters/models.py
@@ -1,3 +1,38 @@
 from django.db import models
 
-# Create your models here.
+
+class Category(models.Model):
+
+    class Meta:
+        verbose_name_plural = 'Categories'
+        
+    name = models.CharField(max_length=254)
+    friendly_name = models.CharField(max_length=254, null=True, blank=True)
+
+    def __str__(self):
+        return self.name
+
+    def get_friendly_name(self):
+        return self.friendly_name
+
+
+COLOR_CHOICES = (
+    ('Color', 'Color'),
+    ('Black & White', 'Black & White'),
+)
+
+
+class Poster(models.Model):
+    category = models.ForeignKey('Category', null=True, blank=True, on_delete=models.SET_NULL)
+    sku = models.CharField(max_length=254, null=True, blank=True)
+    name = models.CharField(max_length=254)
+    description = models.TextField()
+    has_color = models.BooleanField(default=False, blank=True, null=True)
+    color_option = models.CharField(max_length=15, blank=True, default='Color', choices=COLOR_CHOICES)
+    price = models.DecimalField(max_digits=6, decimal_places=2)
+    rating = models.DecimalField(max_digits=6, decimal_places=2, null=True, blank=True)
+    image_url = models.URLField(max_length=1024, null=True, blank=True)
+    image = models.ImageField(null=True, blank=True)
+
+    def __str__(self):
+        return self.name
