e1da45ad84bdc0b7571108cff55c65c4cc485169
sharondrinkwaard <97448501+sharondrinkwaard@users.noreply.github.com>
2024-02-22 13:22:01 +0000

    add urls and view to render posters

--- posters/views.py
+++ posters/views.py
@@ -1,3 +1,15 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404, redirect
+from django.views import generic, View
+from .models import Poster
 
-# Create your views here.
+
+def all_posters(request):
+    """ A view to display all posters on a page """
+
+    posters = Poster.objects.all()
+
+    context = {
+        'posters': posters,
+    }
+
+    return render(request, 'posters/posters.html')
