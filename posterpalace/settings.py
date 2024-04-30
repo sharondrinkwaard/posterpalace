d616af594b49eba938f1db887692babeacb89552
sharondrinkwaard <sharondrinkwaard@gmail.com>
2024-04-30 15:21:36 +0000

    attempt to fix staticfiles

--- posterpalace/settings.py
+++ posterpalace/settings.py
@@ -168,7 +168,7 @@ MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 # AWS
 if 'USE_AWS' in os.environ:
     # Bucket Config
-    AWS_STORAGE_BUCKET_NAME = 'posterpalace-policy'
+    AWS_STORAGE_BUCKET_NAME = 'posterpalace'
     AWS_S3_REGION_NAME = 'eu-north-1'
     AWS_ACCESS_KEY_ID = os.environ.get('AWS_ACCESS_KEY_ID')
     AWS_SECRET_ACCESS_KEY = os.environ.get('AWS_SECRET_ACCESS_KEY')
