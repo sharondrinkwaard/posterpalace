b2eff9adc2486742ac91f34662325ca28da31bba
Sharon Drinkwaard <sharondrinkwaard@gmail.com>
2024-07-04 11:19:49 +0000

    Fix function to delete cart item

--- cart/views.py
+++ cart/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render, get_object_or_404, redirect
+from django.shortcuts import render, get_object_or_404, redirect, HttpResponse
 from django.views import generic, View
 from posters.models import Poster
 
