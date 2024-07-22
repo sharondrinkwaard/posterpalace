d34695fcfa81c2e6c6d562f251f799778e3091c3
Sharon Drinkwaard <sharondrinkwaard@gmail.com>
2024-07-22 11:52:34 +0000

    add payments core functionality

--- checkout/views.py
+++ checkout/views.py
@@ -2,20 +2,34 @@ from django.conf import settings
 from .forms import OrderForm
 from django.contrib import messages
 from django.shortcuts import render, redirect, reverse, get_object_or_404, HttpResponse
+from cart.contexts import shopping_content
+import stripe
 
 
 def checkout(request):
+    stripe_public_key = settings.STRIPE_PUBLIC_KEY
+    stripe_secret_key = settings.STRIPE_SECRET_KEY
+
     cart = request.session.get('cart', {})
     if not cart:
         messages.error(request, "There's nothing in your cart")
         return redirect(reverse('posters'))
 
+    current_cart = shopping_content(request)
+    total = current_cart['total']
+    stripe_total = round(total * 100)
+    stripe.api_key = stripe_secret_key
+    intent = stripe.PaymentIntent.create(
+        amount=stripe_total,
+        currency=settings.STRIPE_CURRENCY,
+    )
+
     order_form = OrderForm()
     template = 'checkout/checkout.html'
     context = {
         'order_form': order_form,
-        'stripe_public_key': 'pk_test_51McQpqGL29wYvReMN5NRcKhCFIuOHM7YVGCyeaPx9ylJIKcwerMaSeEw56Ww8aQfTeWobAedsQguQzNQGku1iDGn00X6cduK9s',
-        'client_secret': 'test client secret',
+        'stripe_public_key': stripe_public_key,
+        'client_secret': intent.client_secret,
     }
 
     return render(request, template, context)
